6591a57b0396c4f1d8d685ccb14c6e3f56ca3e25
tnabror <abrorsmasager@gmail.com>
2017-09-17 21:55:45 +0700

    testing fb login

--- src/test/java/com/test/selenium/selenium_test/AppTest.java
+++ src/test/java/com/test/selenium/selenium_test/AppTest.java
@@ -1,5 +1,12 @@
 package com.test.selenium.selenium_test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -33,6 +40,32 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+    	// Initial Web Driver
+    	WebDriver driver = new FirefoxDriver();
+    	
+    	// Open FB
+    	driver.get("https://www.facebook.com/");
+    	
+    	WebDriverWait dWait = new WebDriverWait(driver, 10);
+    	dWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='email']")));
+    	
+    	//Login
+    	WebElement fEmail = driver.findElement(By.xpath(".//*[@id='email']"));
+    	WebElement fPass = driver.findElement(By.xpath(".//*[@id='pass']"));
+    	WebElement bLogin = driver.findElement(By.xpath(".//*[@id='u_0_2']"));
+    	
+    	
+    	fEmail.sendKeys("a8_r0r@yahoo.co.id");
+    	fPass.sendKeys("ulfamardiyah");
+    	bLogin.click();
+    	
+    	dWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='u_0_1d']/div/div[2]/textarea")));
+    	WebElement fSuccesLogin = driver.findElement(By.xpath(".//*[@id='u_0_1d']/div/div[2]/textarea"));
+
+    	assertTrue(fSuccesLogin.isDisplayed());
+    	if(fSuccesLogin.isDisplayed()){
+    		System.out.println("Sukses");
+    	}
+    	driver.close();
     }
 }
